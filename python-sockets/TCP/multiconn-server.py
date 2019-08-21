6a95e6c6a423a21fb62a29d38573e3c439795d9f
dratani <dratani@gmail.com>
2019-08-21 16:46:41 -0500

    Multiconexión usando selectores

--- python-sockets/TCP/multiconn-server.py
+++ python-sockets/TCP/multiconn-server.py
@@ -1,61 +1,37 @@
 #!/usr/bin/env python3
-
+#
 import sys
 import socket
 import selectors
 import types
-
+#
 sel = selectors.DefaultSelector()
 
-
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
+def accept(sock, mask):
+    conn, addr = sock.accept()  # Should be ready
+    print('accepted', conn, 'from', addr)
     conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+    sel.register(conn, selectors.EVENT_READ, read)
 
+def read(conn, mask):
+    data = conn.recv(1000)  # Should be ready
+    if data:
+        print('echoing', repr(data), 'to', conn)
+        conn.send(data)  # Hope it won't block
+    else:
+        print('closing', conn)
+        sel.unregister(conn)
+        conn.close()
 
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print("closing connection to", data.addr)
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print("echoing", repr(data.outb), "to", data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+with socket.socket() as sock:
+    sock.bind(('localhost', 1234))
+    sock.listen(100)
+    sock.setblocking(False)
+    sel.register(sock, selectors.EVENT_READ, accept)
 
-
-if len(sys.argv) != 3:
-    print("usage:", sys.argv[0], "<host> <port>")
-    sys.exit(1)
-
-host, port = sys.argv[1], int(sys.argv[2])
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print("listening on", (host, port))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-
-try:
     while True:
-        events = sel.select(timeout=None)
+        print("Esperando evento...")
+        events = sel.select()
         for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
-except KeyboardInterrupt:
-    print("caught keyboard interrupt, exiting")
-finally:
-    sel.close()
+            callback = key.data
+            callback(key.fileobj, mask)
