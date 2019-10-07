e8a098470d6417ccc2d5d2f8f1d03fe51d1993e3
dratani <dratani@gmail.com>
2019-10-07 10:35:24 -0500

    Aplicaciones Internet HTTP

---
+++ Aplicaciones de Internet/HTTPClient.py
@@ -0,0 +1,18 @@
+import argparse
+import http.client
+REMOTE_SERVER_HOST = 'https://www.python.org/'
+REMOTE_SERVER_PATH = '/'
+
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+data1 = r1.read()  # This will return entire content.
+conn.request("GET", "/")
+r1 = conn.getresponse()
+while True:
+     chunk = r1.read(200)  # 200 bytes
+     if not chunk:
+          break
+     print(repr(chunk))
---
+++ Aplicaciones de Internet/geocoding.py
@@ -0,0 +1,7 @@
+import geocoder
+g = geocoder.osm('ESCOM, MX')
+print(g.ok)
+print(g.geojson)
+print(g.json)
+print (g.wkt)
+print (g.osm)
