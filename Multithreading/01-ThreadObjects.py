d9154d913415f78158c95f00a8fe4cb46867ba46
tani <dratani@gmail.com>
2021-09-23 01:03:41 -0500

    Actualizado

--- Multithreading/01-ThreadObjects.py
+++ Multithreading/01-ThreadObjects.py
@@ -1,12 +1,18 @@
 import threading
+import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+                    )
 def worker(num):
     """hilo función trabajador"""
-    print ('Trabajador',num)
+    print ('Trabajador', num)
+
+    logging.debug("Función worker")
     return
 
 threads = []
 for i in range(5):
-    t = threading.Thread(target=worker,args=(i,))
+    t = threading.Thread(target=worker, args=(i,))
     threads.append(t)
     t.start()
--- Multithreading/02-CurrentThread.py
+++ Multithreading/02-CurrentThread.py
@@ -1,16 +1,21 @@
 import threading
 import time
-
-
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format="[%(threadName)-10s] %(message)s")
 def worker():
-    print (threading.currentThread().getName(), 'Iniciando')
+    print (threading.currentThread().name, 'Iniciando')
+    logging.debug("Iniciando")
     time.sleep(2)
-    print (threading.currentThread().getName(), 'Terminando')
+    logging.debug("Terminando")
+ #   print (threading.currentThread().name, 'Terminando')
 
 def my_service():
-    print (threading.currentThread().getName(), 'Iniciando')
+  #  print (threading.currentThread().name, 'Iniciando')
+    logging.debug("Iniciando")
     time.sleep(3)
-    print (threading.currentThread().getName(), 'Terminando')
+  #  print (threading.currentThread().name, 'Terminando')
+    logging.debug("Terminando")
 
 t = threading.Thread(name='Servicio 1', target=my_service)
 w = threading.Thread(name='Trabajador', target=worker)
