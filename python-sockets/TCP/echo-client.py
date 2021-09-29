e930e64aaebf6500ae4650603229716320a76439
tani <dratani@gmail.com>
2021-09-28 22:37:55 -0500

    Cliente TCP envía Libro

--- python-sockets/TCP/echo-client.py
+++ python-sockets/TCP/echo-client.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
 import socket
-
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+import time
+HOST = "127.0.0.1"  # El hostname o la IP del servidor
+PORT = 12345  # El puerto que usa el servidor
 buffer_size = 1024
+archivo = open("../../libros/MobyDick.txt", "r")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
     TCPClientSocket.connect((HOST, PORT))
-    print("Enviando mensaje...")
-    TCPClientSocket.sendall(b"Hello TCP server")
-    print("Esperando una respuesta...")
-    data = TCPClientSocket.recv(buffer_size)
-    print("Recibido,", repr(data), " de", TCPClientSocket.getpeername())
+    for linea in archivo.readlines():
+        print("Enviando mensaje",linea)
+        TCPClientSocket.sendall(str.encode(linea))
+        print("Esperando una respuesta...")
+        data = TCPClientSocket.recv(buffer_size)
+        print("Recibido,", repr(data), " de", TCPClientSocket.getpeername())
+       # time.sleep(1)
+    archivo.close()
