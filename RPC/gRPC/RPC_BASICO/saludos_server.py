892041c7557002cfce40473b11f781a186df723d
tani <dratani@gmail.com>
2021-12-02 08:30:05 -0600

    gRPC

--- RPC/gRPC/RPC_BASICO/saludos_server.py
+++ RPC/gRPC/RPC_BASICO/saludos_server.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The Python implementation of the GRPC helloworld.Greeter server."""
+"""The Python implementation of the GRPC saludos.Saludar server."""
 
 from concurrent import futures
 import logging
