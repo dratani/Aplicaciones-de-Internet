ce0ba0e75b921d8cd5ec19c964afd14b18ee5c03
tani <dratani@gmail.com>
2021-12-02 08:24:58 -0600

    gRPC

---
+++ RPC/gRPC/RPC_BASICO/saludos_client.py
@@ -0,0 +1,37 @@
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the GRPC helloworld.Greeter client."""
+
+from __future__ import print_function
+
+import logging
+
+import grpc
+import saludos_pb2
+import saludos_pb2_grpc
+
+
+def run():
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = saludos_pb2_grpc.SaludosStub(channel)
+        response = stub.DecirHola(saludos_pb2.SolicitudSaludo(nombre='Tani'))
+    print("Solicitud de saludo recibida: " + response.saludo)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
